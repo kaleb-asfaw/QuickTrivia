72423574466dafa81ad6f404335dbe0417cd0a19
kaleb-asfaw <kda@mit.edu>
2024-06-24 19:00:20 -0400

    corrected name

--- game.py
+++ game.py
@@ -1,17 +1,18 @@
-from api import get_data as api
+from api import get_trivia_questions as api
 import constants as c
 import messages as m
 import random
 
 username = str(input("Hello! To begin, please enter a username: "))
 
-print(m.print_categories)
+print(m.print_categories(c.CATEGORIES))
 category = input("Choose the category you'd like to play: ")
 while category not in c.NUMS:
     category = input("Please choose a valid category. See the options above and select a corresponding number (1-24): ")
 
 # Here is the trivia match data that should be relayed to users one question at a time
-response = api.get_data(int(category)+8)
+response = api.get_trivia_questions(int(category)+8)
+
 
 # Now, we process the response
 for i, q_data in enumerate(response.json()["results"]):
