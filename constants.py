1afc746794f5446d933152d34e5f4e96ef390913
kaleb-asfaw <kda@mit.edu>
2024-06-24 18:32:47 -0400

    game logic that incorporates API response logic

---
+++ constants.py
@@ -0,0 +1,30 @@
+# ADD 8 TO ALL OF THE ID's
+CATEGORIES = [
+    ("General Knowledge", 1),
+    ("Entertainment: Books", 2),
+    ("Entertainment: Film", 3),
+    ("Entertainment: Music", 4),
+    ("Entertainment: Musicals & Theatres", 5),
+    ("Entertainment: Television", 6),
+    ("Entertainment: Video Games", 7),
+    ("Entertainment: Board Games", 8),
+    ("Science & Nature", 9),
+    ("Science: Computers", 10),
+    ("Science: Mathematics", 11),
+    ("Mythology", 12),
+    ("Sports", 13),
+    ("Geography", 14),
+    ("History", 15),
+    ("Politics", 16),
+    ("Art", 17),
+    ("Celebrities", 18),
+    ("Animals", 19),
+    ("Vehicles", 20),
+    ("Entertainment: Comics", 21),
+    ("Science: Gadgets", 22),
+    ("Entertainment: Japanese Anime & Manga", 23),
+    ("Entertainment: Cartoon & Animations", 24)
+]
+NUMS = [str(i) for i in range(1, 25)]
+
+TYPE = 'multiple'
--- game.py
+++ game.py
@@ -1,5 +1,34 @@
-# Kaleb Day 1
+from api import get_data as api
+import constants as c
+import messages as m
+import random
 
+username = str(input("Hello! To begin, please enter a username: "))
 
-def test():
-    pass
+print(m.print_categories)
+category = input("Choose the category you'd like to play: ")
+while category not in c.NUMS:
+    category = input("Please choose a valid category. See the options above and select a corresponding number (1-24): ")
+
+# Here is the trivia match data that should be relayed to users one question at a time
+response = api.get_data(int(category)+8)
+
+# Now, we process the response
+for i, q_data in enumerate(response.json()["results"]):
+    question = q_data["question"]
+    ans = q_data["correct_answer"]
+    options = q_data["incorrect_answers"] + [ans]
+    random.shuffle(options)
+
+    print(f"Question {i+1}: {question}")
+    mapping = m.print_options(options, ans)
+    choice = input("Enter your answer (a, b, c, or d): ")
+    while choice not in ["a", "b", "c", "d"]:
+        choice = input("Invalid selection, please enter one of the following: a, b, c, or d: ")
+
+    if mapping[choice] == "CORRECT":
+        m.print_correct_guess()
+    else:
+        m.print_incorrect_guess()
+
+m.print_endgame(category)
