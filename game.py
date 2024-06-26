963f3d8a30bb7bd274e3f08ceb1aadee9feddf33
kaleb-asfaw <kda@mit.edu>
2024-06-26 16:28:43 -0400

    fix: input restrictions added + basic global leaderboard for Top 10 has been added

--- game.py
+++ game.py
@@ -1,19 +1,17 @@
-import firebase_admin
-from firebase_admin import credentials, firestore
 import leaderboard as db
 from api import get_trivia_questions
 import constants as c
-import messages as m
+import functions as f
 import loc_scoreboard as sb
 import random
 
 # We will store the username in a local database as well as the total points scored
 # If the username is already in the database the total score will be updated
-username = str(input("Hello! To begin, please enter a username> "))
+username = f.get_username()
 points = 0; # points scored in this game session
 num_questions = 0
 
-category = m.print_categories(c.CATEGORIES)
+category = f.print_categories(c.CATEGORIES)
 while category not in c.NUMS:
     category = input("Please choose a valid category. See the options above and select a corresponding number (1-24): ")
 
@@ -29,18 +27,18 @@ for i, q_data in enumerate(response["results"]):
     random.shuffle(options)
 
     print(f"Question {i+1}: {question}")
-    mapping = m.print_options(options, ans)
+    mapping = f.print_options(options, ans)
     choice = input("Enter your answer (a, b, c, or d): ")
-    while choice not in ["a", "b", "c", "d"]:
+    while choice.lower() not in ["a", "b", "c", "d"]:
         choice = input("Invalid selection, please enter one of the following: a, b, c, or d: ")
 
     num_questions += 1
-    if mapping[choice] == "CORRECT":
-        m.print_correct_guess()
+    if mapping[choice.lower()] == "CORRECT":
+        f.print_correct_guess()
         points += 1
         print("You got one point! Current points: ", points)
     else:
-        m.print_incorrect_guess()
+        f.print_incorrect_guess()
         print("No point for you :( Current points: ", points)
 
 # update the scoreboard
@@ -49,8 +47,9 @@ sb.update_scoreboard(username, points)
 db.add_score(username, points)
 
 #print ending message and current leaderboard
-m.print_endgame()
+f.print_endgame()
 print()
 print(f"Congratulations, you got {points} / {num_questions} questions!")
 sb.print_scoreboard()
-m.print_endgame()
+f.print_global_leaderboard()
+f.print_endgame()
