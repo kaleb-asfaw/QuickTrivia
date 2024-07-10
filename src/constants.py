ff4b9bf2c59b25bdd9739da3b7ba6e13713b8cb4
kaleb-asfaw <kda@mit.edu>
2024-07-10 09:29:23 -0400

    constant update (map Id to name of category)

--- src/constants.py
+++ src/constants.py
@@ -25,7 +25,15 @@ CATEGORIES = [
     ("Entertainment: Japanese Anime & Manga", 23),
     ("Entertainment: Cartoon & Animations", 24)
 ]
+ID_TO_CATEGORIES = {1: 'General Knowledge', 2: 'Entertainment: Books', 3: 'Entertainment: Film', 4: 'Entertainment: Music', 5: 'Entertainment: Musicals & Theatres', 6: 'Entertainment: Television', 7: 'Entertainment: Video Games', 8: 'Entertainment: Board Games', 9: 'Science & Nature', 10: 'Science: Computers', 11: 'Science: Mathematics', 12: 'Mythology', 13: 'Sports', 14: 'Geography', 15: 'History', 16: 'Politics', 17: 'Art', 18: 'Celebrities', 19: 'Animals', 20: 'Vehicles', 21: 'Entertainment: Comics', 22: 'Science: Gadgets', 23: 'Entertainment: Japanese Anime & Manga', 24: 'Entertainment: Cartoon & Animations'}
 NUMS = [str(i) for i in range(1, 25)]
 
 PROHIBITED = {'global_best', 'oj', 'OJ', 'Oj'}
 PROTECTED = {"oJ"}
+
+def fun():
+    DIC = {}
+    for name, ID in CATEGORIES:
+        DIC[ID] = name
+    print(DIC)
+fun()
